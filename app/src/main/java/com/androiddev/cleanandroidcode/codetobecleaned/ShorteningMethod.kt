254f6a737ef930754e874309868f6a031e4d8ddb
Sumit Kumar Tiwary <sumitkrnitjsr@gmail.com>
2022-05-18 22:49:32 +0530

    Init Project

---
+++ app/src/main/java/com/androiddev/cleanandroidcode/codetobecleaned/ShorteningMethod.kt
@@ -0,0 +1,8 @@
+package com.androiddev.cleanandroidcode.codetobecleaned
+
+class DemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
